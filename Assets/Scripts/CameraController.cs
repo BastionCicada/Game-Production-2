6cc2bd2585b931045546bd01ca4062a9da0fe488
BastionCicada <118742807+BastionCicada@users.noreply.github.com>
2023-10-16 16:57:50 +0100

    Test 1 off Camera Controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+  
+  public float mouseSensitivity;
+  
+  private float rotationY;
+  private float rotationX;
 
+  public Transform Target;
+
+  public float distanceFromTarget;
+  
+  private Vector3 activeRotation;
+  private Vector3 smoothVelocity = Vector3.zero;
+
+  public float smoothTime = 0.2f;
+
+  public Vector2 rotationXMinMax = new Vector2(-20,20);
+
+  
     // Update is called once per frame
     void Update()
     {
-        
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        rotationY += mouseX;
+        rotationX += mouseY;
+
+        rotationX = Mathf.Clamp(rotationX, rotationXMinMax.x, rotationXMinMax.y);
+
+        Vector3 nextRotation = new Vector3(rotationX, rotationY);
+
+        activeRotation = Vector3.SmoothDamp(activeRotation, nextRotation, ref smoothVelocity, smoothTime);
+        transform.localEulerAngles = activeRotation;
+
+        transform.position = Target.position - transform.forward * distanceFromTarget;
     }
 }
