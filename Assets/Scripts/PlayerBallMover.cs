bb1c3350baf02676725c6fed269b712cfecb259e
BastionCicada <118742807+BastionCicada@users.noreply.github.com>
2023-10-24 16:55:57 +0100

    Movement controller

---
+++ Assets/Scripts/PlayerBallMover.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBallMover : MonoBehaviour
+{
+    private float maxSpeed;
+    public Rigidbody rigid;
+    public float currentSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxis("Horizontal") > 0)
+        {
+            rigid.AddForce(Vector3.right * currentSpeed); // adds force going right
+        }
+        else if (Input.GetAxis("Horizontal") < 0) 
+        {
+            rigid.AddForce(Vector3.left * currentSpeed); // adds for going left by subtracting the force
+
+        }
+
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            rigid.AddForce(Vector3.forward * currentSpeed);
+        }
+        else if (Input.GetAxis("Vertical") < 0)
+        {
+            rigid.AddForce(Vector3.back * currentSpeed);
+        }
+    }
+}
